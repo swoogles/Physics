75b58653249951958a261efdc84883de751993ac
Bill Frasure <bill.frasure@gmail.com>
2013-06-23 14:59:26 -0600

    -Still figuring out Octree cube-quadrant regions

--- src/Parallelization/Quadrant.cpp
+++ src/Parallelization/Quadrant.cpp
@@ -52,10 +52,6 @@ void Quadrant::subdivideAll( int levels, int numCells )
   sgVec4 newPos;
   sgVec3 newDimensions;
 
-  newDimensions[0] = dimensions[0]/2;
-  newDimensions[1] = dimensions[1]/2;
-  newDimensions[2] = dimensions[2]/2;
-
   int xFactor;
   int yFactor;
   int zFactor;
@@ -67,43 +63,48 @@ void Quadrant::subdivideAll( int levels, int numCells )
   if ( this->level < levels )
   {
 
-  for ( int x = 0; x < 2; x++ )
-  {
-    for ( int y = 0; y < 2; y++ )
+    for ( int x = 0; x < 2; x++ )
     {
-      for ( int z = 0; z < 2; z++ )
+      for ( int y = 0; y < 2; y++ )
       {
-        if ( x ) {
-          xFactor=1;
-        }
-        else {
-          xFactor =-1;
-        }
-        if ( y ) {
-          yFactor=1;
-        }
-        else {
-          yFactor =-1;
-        }
-        if ( z ) {
-          zFactor=1;
-        }
-        else {
-          zFactor =-1;
-        }
-        newPos[0]=pos[0]+(xFactor*newDimensions[0]/2);
-        newPos[1]=pos[1]+(yFactor*newDimensions[1]/2);
-        newPos[2]=pos[2]+(zFactor*newDimensions[2]/2);
+        for ( int z = 0; z < 2; z++ )
+        {
+          if ( x ) {
+            xFactor=1;
+          }
+          else {
+            xFactor =-1;
+          }
+          if ( y ) {
+            yFactor=1;
+          }
+          else {
+            yFactor =-1;
+          }
+          if ( z ) {
+            zFactor=1;
+          }
+          else {
+            zFactor =-1;
+          }
+          newPos[0]=pos[0]+(xFactor*dimensions[0]);
+          newPos[1]=pos[1]+(yFactor*dimensions[1]);
+          newPos[2]=pos[2]+(zFactor*dimensions[2]);
 
-        //targentQuadrant 
-        //quadOctree->subdivide(i,j,k,4);
-        quadOctree->set( x, y, z, new Quadrant( numCells, this->level + 1, newPos, newDimensions ) );
-        targetQuadrant = quadOctree->at(x,y,z);
-        targetQuadrant->subdivideAll(levels, 4);
+          newDimensions[0] = dimensions[0]/2;
+          newDimensions[1] = dimensions[1]/2;
+          newDimensions[2] = dimensions[2]/2;
 
+
+          //targentQuadrant 
+          //quadOctree->subdivide(i,j,k,4);
+          quadOctree->set( x, y, z, new Quadrant( numCells, this->level + 1, newPos, newDimensions ) );
+          targetQuadrant = quadOctree->at(x,y,z);
+          targetQuadrant->subdivideAll(levels, 4);
+
+        }
       }
     }
-  }
 
   }
 }
