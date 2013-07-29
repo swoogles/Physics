e0056beeacef676a3fb38790dd9bc3a75eeb6668
Bill Frasure <bill.frasure@gmail.com>
2013-07-28 22:29:36 -0600

    -Compiling on the laptop! Woot woot!

--- Release/objects.mk
+++ Release/objects.mk
@@ -4,4 +4,8 @@
 
 USER_OBJS :=
 
-LIBS := -lglut -ljpeg -lplibpuaux -lplibpu -lplibsg
+# 64 Bit/Desktop version
+# LIBS := -lglut -ljpeg -lplibpuaux -lplibpu -lplibsg
+
+# 32 Bit/Laptop version
+LIBS := -lGL -lGLU -lglut -ljpeg -lplibpuaux -lplibpu -lplibsg -lplibul -lplibfnt
