ae43fec1483514b3ee35bdcefb23f732d4dda386
Bill Frasure <bill.frasure@gmail.com>
2013-08-08 13:57:09 -0600

    PHYS-3 #time 20m #comment Explored Boost Multi-Arrays more

--- boostMultiArray.cpp
+++ boostMultiArray.cpp
@@ -15,15 +15,26 @@ int main(int argcp, char **argv)
     typedef multi_array<float, 3> array_type;
     typedef array_type::index index;
 
-    array_type  * quadOctreeMine = new array_type(boost::extents[2][2][2]);
+    array_type * quadOctreeMine = new array_type(boost::extents[2][2][2]);
     cout << "quadOctreeMine: " << quadOctreeMine << endl;
     quadOctreeMine = new array_type(boost::extents[2][2][2]);
     cout << "quadOctreeMine: " << quadOctreeMine << endl;
-    quadOctreeMine = new array_type(boost::extents[2][2][2]);
+    quadOctreeMine = new array_type(boost::extents[2][3][4]);
     cout << "quadOctreeMine: " << quadOctreeMine << endl;
-    quadOctreeMine = new array_type(boost::extents[2][2][2]);
-    cout << "quadOctreeMine: " << quadOctreeMine << endl;
-    quadOctreeMine = new array_type(boost::extents[2][2][2]);
-    cout << "quadOctreeMine: " << quadOctreeMine << endl;
+
+    cout << "numDimensions" << quadOctreeMine->num_dimensions() << endl;
+
+    for ( int i = 0; i < quadOctreeMine->num_dimensions(); i++ )
+    {
+      cout << "Dimensions[" << i << "] size: " <<  quadOctreeMine->shape()[i] << endl;
+      cout << "Strides[" << i << "] size: " <<  quadOctreeMine->strides()[i] << endl;
+      cout << endl;
+    }
+
+    cout << "NumElements: " << quadOctreeMine->num_elements() << endl;
+    cout << "size: " << quadOctreeMine->size() << endl;
+
+    iterator curIt = quadOctreeMine->begin();
+
 }
 
