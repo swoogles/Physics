f691e449ddcf2d5c2fad986af485125b476cde00
swoogles <bill.frasure@gmail.com>
2014-01-07 00:44:16 -0700

    PHYS-23 #time 15m #comment Added some very simple variables to a few makefiles. Still needs to improve a ton.

--- Release_Clang/src/subdir.mk
+++ Release_Clang/src/subdir.mk
@@ -24,11 +24,12 @@ CPP_DEPS += \
 ./src/main.d \
 ./src/menus.d 
 
+CPLUSPLUS_COMPILER=GCC
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
-	@echo 'Invoking: GCC C++ Compiler'
+	@echo 'Invoking: $(CPLUSPLUS_COMPILER) C++ Linker'
 	clang++ -std=c++0x  -Wall  -g -fopenmp -Imgl -Iplibsg -Iplibul -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
