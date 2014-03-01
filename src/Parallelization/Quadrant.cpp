587f22a0e472575e9a92064f69fe1fd003e8ef85
swoogles <bill.frasure@gmail.com>
2014-03-01 13:43:39 -0700

    Use vectors and their functions more in Quadrant
    PHYS-16 #time 40m

--- src/Parallelization/Quadrant.cpp
+++ src/Parallelization/Quadrant.cpp
@@ -64,6 +64,10 @@ void Quadrant::subDivideAll( int levels, int numCells )
 {
   sgVec4 newPos;
   sgVec4 newDimensions;
+  sgVec4 scaleFactors;
+  sgVec4 offsets;
+  sgVec4 halfNewDimensions;
+  sgVec3 crossProduct;
 
   int xFactor, yFactor, zFactor;
 
@@ -78,33 +82,25 @@ void Quadrant::subDivideAll( int levels, int numCells )
       {
         for ( int z = 0; z < 2; z++ )
         {
-          if ( x ) {
-            xFactor=1;
-          }
-          else {
-            xFactor =-1;
-          }
-          if ( y ) {
-            yFactor=1;
-          }
-          else {
-            yFactor =-1;
-          }
-          if ( z ) {
-            zFactor=1;
-          }
-          else {
-            zFactor =-1;
-          }
+          scaleFactors[0] = ( x ) ?  1 : -1;
+          scaleFactors[1] = ( y ) ?  1 : -1;
+          scaleFactors[2] = ( z ) ?  1 : -1;
 
-          newPos[0]=pos[0]+(xFactor*dimensions[0]/4.0);
-          newPos[1]=pos[1]+(yFactor*dimensions[1]/4.0);
-          newPos[2]=pos[2]+(zFactor*dimensions[2]/4.0);
+          // sgScaleVec3( offsets, scaleFactors, 
+          // newPos[0]=pos[0]+(xFactor*dimensions[0]/4.0);
+           
+          sgScaleVec3  ( newDimensions, dimensions, .5 );
+          sgScaleVec3  ( halfNewDimensions, newDimensions, .5 );
+
+          
+              // void sgVectorProductVec3 ( sgVec3 dst, sgVec3 a, sgVec3 b )
+          sgVectorProductVec3 ( crossProduct, scaleFactors, halfNewDimensions );
+
+          newPos[0]=pos[0]+( crossProduct[0] );
+          newPos[1]=pos[1]+( crossProduct[1] );
+          newPos[2]=pos[2]+( crossProduct[2] );
           newPos[3]=1;
 
-          newDimensions[0] = dimensions[0]/2;
-          newDimensions[1] = dimensions[1]/2;
-          newDimensions[2] = dimensions[2]/2;
 
           quad_pointer insertionQuadrant;
           quadOctree[x][y][z] = make_shared<Quadrant>( numCells, this->level + 1, boost::ref(newPos), boost::ref(newDimensions) );
@@ -205,8 +201,16 @@ void Quadrant::insertShape( shape_pointer insertedShape )
 // Guaranteed to hand back an instantiated Quadrant
 boost::shared_ptr<Quadrant> Quadrant::determineShapeQuadrant( shape_pointer shapeToInsert )
 {
-  sgVec4 insertPos, newPos, newDimensions;
+  sgVec4 insertPos;
+  sgVec4 newPos;
+  sgVec4 newDimensions;
+  sgVec4 scaleFactors;
+  sgVec4 offsets;
+  sgVec4 halfNewDimensions;
+  sgVec3 crossProduct;
+
   shapeToInsert->getPos( insertPos ); 
+
   float insertX = insertPos[0];
   float insertY = insertPos[1];
   float insertZ = insertPos[2];
@@ -214,20 +218,25 @@ boost::shared_ptr<Quadrant> Quadrant::determineShapeQuadrant( shape_pointer shap
   sgCopyVec4( newDimensions, dimensions );
   sgScaleVec4 ( newDimensions, .5 );
 
-  float xOffset = dimensions[0]/4;
-  float minXBoundary = pos[0] - newDimensions[0];
+  
+  sgScaleVec3( offsets, dimensions, .25 );
+  // Boundaries [0]=min : [1]=central : [2]=max
+  sgVec3 xBoundaries;
+  // xBoundaries[0]
+  sgVec3 maxBoundaries;
+  sgAddVec3( maxBoundaries, pos, newDimensions );
+
+  sgVec3 minBoundaries;
+  sgSubVec3( minBoundaries, pos, newDimensions );
+
   float centralXBoundary = pos[0];
-  float maxXBoundary = pos[0] + newDimensions[0];
+  float maxXBoundary = maxBoundaries[0];
 
-  float yOffset = dimensions[1]/4;
-  float minYBoundary = pos[1] - newDimensions[1];
   float centralYBoundary = pos[1];
-  float maxYBoundary = pos[1] + newDimensions[1];
+  float maxYBoundary = maxBoundaries[1];
 
-  float zOffset = dimensions[2]/4;
-  float minZBoundary = pos[2] - newDimensions[2];
   float centralZBoundary = pos[2];
-  float maxZBoundary = pos[2] + newDimensions[2];
+  float maxZBoundary = maxBoundaries[2];
 
   int targetX = INVALID_OCTREE_INDEX;
   int targetY = INVALID_OCTREE_INDEX;
@@ -235,45 +244,45 @@ boost::shared_ptr<Quadrant> Quadrant::determineShapeQuadrant( shape_pointer shap
 
 
   // X coordinate checking
-  if ( insertX >= minXBoundary ) //Within min X boundary
+  if ( insertX >= minBoundaries[0] ) //Within min X boundary
   {
     if ( insertX < centralXBoundary ) //Less than central X boundary
     {
       targetX = 0; 
-      newPos[0] = centralXBoundary - xOffset;
+      newPos[0] = centralXBoundary - offsets[0];
     }
-    else if ( insertX <= maxXBoundary ) //Less than max X boundary
+    else if ( insertX <= maxBoundaries[0] ) //Less than max X boundary
     {
       targetX = 1;
-      newPos[0] = centralXBoundary + xOffset;
+      newPos[0] = centralXBoundary + offsets[0];
     }
 
     // Y coordinate checking
-    if ( insertY >= minYBoundary ) //Within min Y boundary
+    if ( insertY >= minBoundaries[2] ) //Within min Y boundary
     {
       if ( insertY < centralYBoundary ) //Less than central Y boundary
       {
         targetY = 0; 
-        newPos[1] = centralYBoundary - yOffset;
+        newPos[1] = centralYBoundary - offsets[1];
       }
-      else if ( insertY <= maxYBoundary ) //Less than max Y boundary
+      else if ( insertY <= maxBoundaries[1] ) //Less than max Y boundary
       {
         targetY = 1;
-        newPos[1] = centralYBoundary + yOffset;
+        newPos[1] = centralYBoundary + offsets[1];
       }
 
       // Z coordinate checking
-      if ( insertZ >= minZBoundary ) //Within min Z boundary
+      if ( insertZ >= minBoundaries[2] ) //Within min Z boundary
       {
         if ( insertZ < centralZBoundary ) //Less than central Z boundary
         {
-          newPos[2] = centralZBoundary - zOffset;
+          newPos[2] = centralZBoundary - offsets[2];
           targetZ = 0; 
         }
-        else if ( insertZ <= maxZBoundary ) //Less than max Z boundary
+        else if ( insertZ <= maxBoundaries[2] ) //Less than max Z boundary
         {
           targetZ = 1;
-          newPos[2] = centralZBoundary + zOffset;
+          newPos[2] = centralZBoundary + offsets[2];
         }
       }
     }
