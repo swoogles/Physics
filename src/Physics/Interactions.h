822c591ef5f2a32b0dc85e03331e3fe5031aaf0c
swoogles <bill.frasure@gmail.com>
2014-03-16 21:45:50 -0600

    Remove transferred methods from Interactions class
    PHYS-16 #time 5m

--- src/Physics/Interactions.h
+++ src/Physics/Interactions.h
@@ -83,44 +83,6 @@ void getVectorToObject2(boost::shared_ptr<MyShape> object1, boost::shared_ptr<My
 void getVectorToQuadrant(boost::shared_ptr<MyShape> object1, boost::shared_ptr<Quadrant> quadrant, sgVec4 sepVector);
 
 
-/*! \relates MyShape
- *  \brief Determines the final angular momentum after 2 objects collide in a completely inelastic collision
- *
- *  The final angular momentum is determined with the following formula:
- *  \n v1i = the initial velocity of object 1
- *  \n v2i = the initial velocity of object 2
- *	\n m1 = the mass of object 1
- *	\n m2 = the mass of object 2
- *	\n e = the coefficient of restitution (e = 1 for elastic collision)
- *	\n n = normal unit vector drawn from object 1 to object 2
- *	\n c = n . (v1i - v2i)
- *	\n v1f = v1i - ((m2c)/(m1 + m2))(1 + e)n
- *	\n v2f = v2i + ((m1c)/(m1 + m2))(1 + e)n
- *
- *	\return The angular momentum to be assigned to the merged object
- */
-void calcMergedAngMomentum(boost::shared_ptr<MyShape> object1, boost::shared_ptr<MyShape> object2, sgVec4 totalAngMom);
-
-/*! \relates Circle
- *  \brief Finds radius after 2 circular objects are merged
- *
- */
-float calcMergedRadius(float massBoth, float density);
-
-
-/*! \relates MyShape
- *  \brief Combines 2 distinct objects in an inelastic collision into the first object and eliminates the second
- *
- *  Combines 2 objects by:
- *  \n -Calculating their combined radius (calcMergedRadius)
- *  \n -Finding their center of mass and centering the new object there
- *  \n -Adding their masses and momentums
- *  \n -Calculating their combined angular momentum about the COM (calcMergedAngMomentum)
- *
- *  Note: This function does NOT delete the second object, currently that must be handled outside
- */
-void mergeObjects(boost::shared_ptr<MyShape> object1, boost::shared_ptr<MyShape> object2);
-
 
 /*! \relates Circle
  * \brief SplitRadius!
