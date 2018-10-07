e9b87fa2c4e8ee6f41a3c8db47a4b9b44faa3b5e
Bill Frasure <bill.frasure@gmail.com>
2018-10-07 09:11:02 -0600

    mk separate unitTests main file, for much quicker re-compilation

---
+++ Test/SimulationTests.cpp
@@ -0,0 +1,50 @@
+#include "catch.hpp"
+
+#include <plib/sg.h>
+
+#include "../src/ShapeFiles/Circle.h"
+#include "../src/Physics/Simulation.h"
+#include "../src/Physics/Simulations.h"
+#include "../src/Physics/Interactions.h"
+
+TEST_CASE( "Items are being removed from Simulation when Octree force calculations are used", "[removing items]" ) {
+    bool testWritten = false;
+    SimulationPointer_t  simulation = Simulations::QuadrantTesting_simplest();
+    // TODO these should *NOT* be necessary after constructing the simulation
+    simulation->updateMinsAndMaxes();
+    simulation->setForceCalcMethod(ForceCalculationMethod::OCTREE);
+    simulation->refreshQuadrant();
+    // TODO put this in Interactions namespace
+    calcForcesAll(simulation);
+    simulation->update();
+    // Even though the simulation shapes list is updated, something is still wrong. Possibly I'm losing the handle on
+    // the shapes elsewhere?
+    REQUIRE(simulation->shapes.getShapes().size() == 1);
+    REQUIRE( testWritten );
+}
+
+TEST_CASE( "Simulation is made", "[simulation]" ) {
+
+    Simulation testSimulation;
+
+    // REQUIRE( testSimulation.getForceCalcMethod() == Simulation::FORCE_CALC_METHOD_OCTREE );
+    REQUIRE( testSimulation.getCurStep() == 0 );
+    REQUIRE( testSimulation.getDT() == 1 );
+    REQUIRE( testSimulation.getTimeElapsed() == 0 );
+    REQUIRE( testSimulation.isPaused() == true );
+    REQUIRE( testSimulation.isAllElastic() == false );
+    REQUIRE( testSimulation.isAllInelastic() == true );
+    REQUIRE( testSimulation.isConstGravField() == false );
+    REQUIRE( testSimulation.isGravBetweenObjects() == true );
+
+    float distance = 3000;
+    SECTION( "Update dimensions" ) {
+        sgVec4 pos;
+        pos[0] = distance;
+        pos[1] = distance;
+        pos[2] = distance;
+        testSimulation.updateXYMinsAndMaxes( pos );
+
+    }
+
+}
