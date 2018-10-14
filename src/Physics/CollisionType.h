927b615d04d1af643e09ff4df3a5039cc7a57fed
Bill Frasure <bill.frasure@gmail.com>
2018-10-14 11:59:45 -0600

    Create CollisionType enum

---
+++ src/Physics/CollisionType.h
@@ -0,0 +1,12 @@
+//
+// Created by bfrasure on 14/10/18.
+//
+
+#ifndef PHYSICS_COLLISIONTYPE_H
+#define PHYSICS_COLLISIONTYPE_H
+
+enum CollisionType {
+    ELASTIC, INELASTIC
+};
+
+#endif //PHYSICS_COLLISIONTYPE_H
