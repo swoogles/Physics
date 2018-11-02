508f1794d2982611a46d34972c6fa7ff68b007b3
bfrasure <bill.frasure@gmail.com>
2018-11-01 22:28:21 -0600

    Create AstronomicalValues class

---
+++ src/Physics/AstronomicalValues.cpp
@@ -0,0 +1,10 @@
+//
+// Created by bfrasure on 10/31/18.
+//
+
+#include "AstronomicalValues.h"
+
+const float AstronomicalValues::MASS_SUN = 598910000;
+
+// VALUES IN g/cm³
+const float AstronomicalValues::DENSITY_SUN = 1.409;
---
+++ src/Physics/AstronomicalValues.h
@@ -0,0 +1,17 @@
+//
+// Created by bfrasure on 10/31/18.
+//
+
+#ifndef PHYSICS_ASTRONOMICALVALUES_H
+#define PHYSICS_ASTRONOMICALVALUES_H
+
+
+class AstronomicalValues {
+public:
+    const static float MASS_SUN;
+    const static float DENSITY_SUN;
+
+};
+
+
+#endif //PHYSICS_ASTRONOMICALVALUES_H
