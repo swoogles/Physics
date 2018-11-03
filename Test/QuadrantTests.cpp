b8559e2453a269856227a89e42c9d3a45fba110b
bfrasure <bill.frasure@gmail.com>
2018-11-02 23:57:51 -0600

    Stub for QuadrantTests

---
+++ Test/QuadrantTests.cpp
@@ -0,0 +1,20 @@
+//
+// Created by bfrasure on 11/2/18.
+//
+
+#include "catch.hpp"
+
+#include "../src/ShapeFiles/TouchingPair.h"
+#include "../src/ShapeFiles/PairCollection.h"
+#include "../src/ShapeFiles/Circle.h"
+#include "../src/Parallelization/Quadrant.h"
+#include "TestUtils.h"
+#include <plib/sg.h>
+
+#include <memory>
+
+TEST_CASE("Get children") {
+    VecStruct pos;
+    Quadrant quadrant(0, )
+
+}
