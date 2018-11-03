193234d3979ce550fcf1be73f84ea45c228383c9
bfrasure <bill.frasure@gmail.com>
2018-11-03 13:11:10 -0600

    Create QuadrantTest sections

--- Test/QuadrantTests.cpp
+++ Test/QuadrantTests.cpp
@@ -17,15 +17,34 @@ TEST_CASE("Get children", "[Quadrant]") {
     VecStruct pos;
     VecStruct dimensions{200, 200, 200};
     Quadrant quadrant(0, pos, dimensions);
-    REQUIRE(quadrant.children().empty());
+    SECTION("Basic properties") {
+        REQUIRE(quadrant.children().empty());
+        REQUIRE(quadrant.getMass() == 0);
+        REQUIRE(quadrant.getShapeInQuadrant() == nullptr);
+    }
 
-    auto a = TestUtils::testCircle();
-    quadrant.insertShape(a);
-    REQUIRE(quadrant.children().size() == 0);
-    auto b = TestUtils::circleAt(10, 5, 2);
-    quadrant.insertShape(b);
-    REQUIRE(quadrant.children().size() == 1);
-    for (const auto & child: quadrant.children()) {
-        cout << "CHILD: " << child << endl;
+    SECTION("Single Insertion") {
+        auto a = TestUtils::testCircle();
+        quadrant.insertShape(a);
+        REQUIRE(quadrant.children().empty());
+    }
+    SECTION("Multiple Insertions") {
+        auto a = TestUtils::testCircle();
+        quadrant.insertShape(a);
+        auto b = TestUtils::circleAt(5, 5, 5);
+        quadrant.insertShape(b);
+        REQUIRE(quadrant.children().size() == 1);
+        quadrant.insertShape(TestUtils::circleAt(-5, 5, 5));
+        quadrant.insertShape(TestUtils::circleAt(5, -5, 5));
+
+        for (const auto &child: quadrant.children()) {
+            cout << "CHILD: " << child << endl;
+        }
+
+        PairCollection deleteList;
+        for (const auto &subQuadrant: quadrant.children()) {
+            REQUIRE_FALSE(subQuadrant == nullptr);
+//        deleteList.insertUniqueElements(calcForceOnObject_Octree(curObject, subQuadrant, 10, recursionLevel + 1)) ;
+        }
     }
 }
