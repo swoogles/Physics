1692f5e2b80f42f17ee0457161706af6c3706b8d
bfrasure <bill.frasure@gmail.com>
2019-01-21 21:34:35 -0700

    Clean up some unused/unneeded Drawing code

--- src/graphics/Drawing.h
+++ src/graphics/Drawing.h
@@ -8,21 +8,7 @@
 
 class Drawing {
 public:
-    /*! \brief The key MyShape function called in the display function
-     *
-     *  Steps:
-     *  \n -Push current matrix onto the stack
-     *  \n -Translate using the pos vector
-     *  \n -Rotate using orientationMat
-     *  \n -Scale using drawScale()
-     *  \n -Call glColor3fv(float *) using color vector
-     *  \n -Call drawUnit() to draw a unit square, circle, etc.
-     *  \n -Pop matrix from the stack
-     */
-    static void draw(shapePointer_t myShape);
     static void draw(Moveable &myShape);
-    static void drawShapes(const vectorT &shapes);
-
 };
 
 #endif
--- src/graphics/GraphicalOperations.cpp
+++ src/graphics/GraphicalOperations.cpp
@@ -10,7 +10,7 @@ using std::size_t;
 
 void drawShapeInQuadrant(Quadrant quadrant) {
     auto shape = quadrant.getShapeInQuadrant();
-    if (shape != nullptr) Drawing::draw(shape);
+    if (shape != nullptr) Drawing::draw(*shape);
 };
 
 void GraphicalOperations::fullQuadrantDrawingFunction(ControlCenter controlCenter, Quadrant quadrant) {
