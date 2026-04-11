fc1faf2dabd9b54f0ca0470801f51f32ffd23584
Bill Frasure <bill.frasure@gmail.com>
2026-04-11 11:54:39 -0600

    Update FfmpegClient.cpp

--- src/graphics/FfmpegClient.cpp
+++ src/graphics/FfmpegClient.cpp
@@ -14,7 +14,7 @@ void FfmpegClient::createVideo(const std::time_t & start) {
     stream << std::put_time(std::localtime(&start), "%F %T");
 //    string ffmpegCommandAndOptions = "ffmpeg  -i ./output/outFrame%05d.jpg -framerate 1 -c:v libx264 -crf 18 -pix_fmt yuv420p \"";
     // Consider 2160p in future
-    string ffmpegCommandAndOptions = "ffmpeg  -i ./output/outFrame%05d.jpg -framerate 1 -c:v libx264 -crf 18 -pix_fmt yuv420p -vf scale=-2:1440:flags=neighbor -r 30 -y \"";
+    string ffmpegCommandAndOptions = "ffmpeg -start_number 1 -i ./output/outFrame%05d.jpg -framerate 1 -c:v libx264 -crf 18 -pix_fmt yuv420p -vf scale=-2:1440:flags=neighbor -r 30 -y \"";
     string outDirectory = "./WorthyVideos/";
     string outVideoFormat = ".mp4\"";
     string fullCommand = ffmpegCommandAndOptions + outDirectory + stream.str() + outVideoFormat;
